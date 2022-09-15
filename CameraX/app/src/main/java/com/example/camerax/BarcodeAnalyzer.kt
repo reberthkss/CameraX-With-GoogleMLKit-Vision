cc538136543f286965414eacedf8994f27b7632c
Reberth <reberthkss@outlook.com>
2022-09-15 00:31:41 -0300

    Smilling detection

---
+++ CameraX/app/src/main/java/com/example/camerax/BarcodeAnalyzer.kt
@@ -0,0 +1,10 @@
+package com.example.camerax
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class BarcodeAnalyzer: ImageAnalysis.Analyzer {
+    override fun analyze(image: ImageProxy) {
+        return
+    }
+}
